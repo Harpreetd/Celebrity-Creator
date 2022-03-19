243fcb5c9b97213f1ee3d444080b8408a59abf5a
Harpreetd <dhimanharpreet17@yahoo.in>
2022-03-19 21:29:44 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let celebrityArray = [
+  { name: "Angelina Jolie", age: 46, profession: "actor", gender: "female" },
+  {
+    name: "Jude Law",
+    age: 49,
+    profession: "actor",
+    gender: "male",
+  },
+  {
+    name: "the Weeknd",
+    age: 32,
+    profession: "singer",
+    gender: "male",
+  },
+  {
+    name: "Ariana Grande",
+    age: 28,
+    profession: "singer",
+    gender: "female",
+  },
+  {
+    name: "Kewin Hart",
+    age: 42,
+    profession: "comedian",
+    gender: "male",
+  },
+];
+console.log(celebrityArray);
+
