19ea00cd015e382b43951fad7a68a99f839e5ffa
Harpreetd <dhimanharpreet17@yahoo.in>
2022-03-26 22:09:18 +0100

    function to show the list according to category

--- index.js
+++ index.js
@@ -1,43 +1,12 @@
+//Oppgave 1
 let celebrityArray = [
   { name: "Angelina Jolie", age: 46, profession: "actor", gender: "female" },
-  {
-    name: "Jude Law",
-    age: 49,
-    profession: "actor",
-    gender: "male",
-  },
-  {
-    name: "the Weeknd",
-    age: 32,
-    profession: "singer",
-    gender: "male",
-  },
-  {
-    name: "Ariana Grande",
-    age: 28,
-    profession: "singer",
-    gender: "female",
-  },
-  {
-    name: "Kewin Hart",
-    age: 42,
-    profession: "comedian",
-    gender: "male",
-  },
+  { name: "Jude Law", age: 49, profession: "actor", gender: "male" },
+  { name: "the Weeknd", age: 32, profession: "singer", gender: "male" },
+  { name: "Ariana Grande", age: 28, profession: "singer", gender: "female" },
+  { name: "Kewin Hart", age: 42, profession: "comedian", gender: "male" },
 ];
 console.log(celebrityArray);
-
-let celebrities;
-function filteredCelebrityInfo(profession) {
-  celebrities = celebrityArray.filter(function (celebrity) {
-    return celebrity.profession == profession;
-  });
-  console.log(celebrities);
-}
-function showCelebrity(celebrities) {
-  let info = document.createElement("h1");
-  document.body.append(info);
-}
 document.querySelector("#actors").addEventListener("click", function () {
   filteredCelebrityInfo("actor");
 });
@@ -47,3 +16,20 @@ document.querySelector("#singers").addEventListener("click", function () {
 document.querySelector("#comedians").addEventListener("click", function () {
   filteredCelebrityInfo("comedian");
 });
+let celebrities;
+// Oppgave 2
+function filteredCelebrityInfo(category) {
+  celebrities = celebrityArray.filter(function (celebrity) {
+    return celebrity.profession == category;
+  });
+  console.log(celebrities);
+  showCelebrity(celebrities);
+}
+
+let celebList = document.getElementById("celeb-list");
+function showCelebrity(celebrities) {
+  celebList.innerHTML = "";
+  for (let i = 0; i < celebrities.length; i++) {
+    celebList.innerHTML += `<div>${celebrities[i].name}</div>`;
+  }
+}
